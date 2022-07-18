ea703f8e4396bc18f0f702f8893e038c6e9e628b
Marcelo Rangel <Marcelo.rcs@icloud.com>
2022-07-18 15:49:24 -0300

    Create "Banco" dataClass and alter Conta.kt

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,12 +5,15 @@ class Pessoa {
     var cpf : String = "123.123.123-12"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
 
 }
 
 fun main() {
     val rangel = Pessoa()
 
-    println(rangel.nome)
-    println(rangel.cpf)
+    println(rangel.pessoaInfo())
 }
