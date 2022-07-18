f4b74e3f6fc59d526031fcd8f6bf2e685253a4c6
Marcelo Rangel <Marcelo.rcs@icloud.com>
2022-07-18 17:25:53 -0300

    Convert pessoa to abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,19 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome : String = "Rangel"
-    var cpf : String = "123.123.123-12"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-
-
-}
-
-fun main() {
-    val rangel = Pessoa()
-
-    println(rangel.pessoaInfo())
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
