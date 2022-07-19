3d2803b3050805c54c98d1ac89e68deee8a1ec5c
Marcelo Rangel <Marcelo.rcs@icloud.com>
2022-07-19 15:32:39 -0300

    Implementing "Autenticacao"

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/FuncionarioGerenteTeste.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/FuncionarioGerenteTeste.kt
@@ -1,16 +1,15 @@
 package one.digitalinnovation.digionebank.testes
 
 
-import one.digitalinnovation.digionebank.Funcionario
 import one.digitalinnovation.digionebank.Gerente
 import one.digitalinnovation.digionebank.ImprimeRelatorioFuncionario
 
 
 fun main(){
-    val daniel = Gerente("Daniel", "123.123.123-22", 4000.0)
+    val daniel = Gerente("Daniel", "123.123.123-22", 4000.0, "senha123")
 
     ImprimeRelatorioFuncionario.imprime(daniel)
-
+    AutenticacaoTeste().autentica(daniel)
 
 }
 
